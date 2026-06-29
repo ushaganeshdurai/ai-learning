e6dcc69978c28752b5607e6a036f8dfb5544cdc9
ushaganeshdurai <gushanandhini2004@gmail.com>
2026-06-29 19:09:13 +0530

    get it

--- Month-1-Fundamentals/Week-2-Math-DS/exercises/pandas_da.py
+++ Month-1-Fundamentals/Week-2-Math-DS/exercises/pandas_da.py
@@ -14,6 +14,22 @@ df = pd.DataFrame( {"name": ["Alice", "Bob", "Charlie"],
     "salary": [50000, 60000, 70000]}
 )
 
-print(f"Shape of dataframe: {df.shape()} {df.dtypes()} ")
+print(f"Shape of dataframe: {df.shape} {df.dtypes} ")
 print(df.head(2))
 print(df["age"])
+
+'''
+Load and Explore Data
+
+1. Load Titanic or iris dataset (use pandas/seaborn)
+2. Print shape
+3. Print first 5 rows
+4. Print info() and describe()
+5. Check data types
+6. Count missing values
+'''
+titanic_df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
+print(titanic_df.shape)
+print("First 5 rows: ",titanic_df.head(5))
+print(titanic_df.info(),titanic_df.describe())
+print()
